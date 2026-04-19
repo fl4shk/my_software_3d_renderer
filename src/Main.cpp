514a4d6fbaf4560a24b21b91e24b48152820f6d5
fl4shk <fl4shk@users.noreply.github.com>
2026-04-19 15:26:28 -0500

    more debug printing yay!

--- src/Main.cpp
+++ src/Main.cpp
@@ -377,6 +377,9 @@ int main(int argc, char** argv) {
                         visib
                     );
                 }
+                mm_printout(
+                    "i=", i, " visib.size(): ", visib.size(), "\n"
+                );
             }
             // TODO: END: later
             //--------
