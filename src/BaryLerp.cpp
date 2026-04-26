d9b889c274b6bcc800c85ffd54a9b08f5b485637
fl4shk <fl4shk@users.noreply.github.com>
2026-04-26 14:50:34 -0500

    more debug printing!

--- src/BaryLerp.cpp
+++ src/BaryLerp.cpp
@@ -44,69 +44,73 @@ BaryLerp::BaryLerp(
 {
 }
 BaryLerp::BaryLerp(
-    //const Vert& vt0,
-    //const Vert& vt1,
-    //const Vert& vt2,
     const std::array<Vert, TRI_VERT_SIZE>& v,
-    //const Tri& tri,
-    //const TriVert& v,
     const std::array<MyRwFixedPt, TRI_VERT_SIZE>& rw_arr,
     const Vec2<MyFixedPt>& v1
-    //bool do_rast_interp
 ) {
-    //const Vert& svt0 = tri.clip_v.at(0);
-    //const Vert& svt1 = tri.clip_v.at(1);
-    //const Vert& svt2 = tri.clip_v.at(2);
     const Vert
-        //& vt0 = tri.screen_v.at(0),
-        //& vt1 = tri.screen_v.at(1),
-        //& vt2 = tri.screen_v.at(2);
         & vt0 = v.at(0),
         & vt1 = v.at(1),
         & vt2 = v.at(2);
 
     const MyRwFixedPt
-        //rwa = tri.rw_arr.at(0),
-        //rwb = tri.rw_arr.at(1),
-        //rwc = tri.rw_arr.at(2);
         rwa = rw_arr.at(0),
         rwb = rw_arr.at(1),
         rwc = rw_arr.at(2);
     const MyFixedPt
         x = v1.x,
         y = v1.y,
-        //rwa = tri.rw_arr.at(0), //MyFixedPt(1) / vt0.v.w,
-        //rwb = tri.rw_arr.at(1), //MyFixedPt(1) / vt1.v.w,
-        //rwc = tri.rw_arr.at(2), //MyFixedPt(1) / vt2.v.w,
-        //wa = svt0.v.w,
-        //wb = svt1.v.w,
-        //wc = svt2.v.w,
-        xa = vt0.v.x, // * rw0,
-        ya = vt0.v.y, // * rw0,
-        za = vt0.v.z, // * rw0,
-        wa = vt0.v.w, // * rw0,
 
-        xb = vt1.v.x, // * rw1,
-        yb = vt1.v.y, // * rw1,
-        zb = vt1.v.z, // * rw1,
-        wb = vt1.v.w, // * rw1,
+        xa = vt0.v.x,
+        ya = vt0.v.y,
+        za = vt0.v.z,
+        wa = vt0.v.w,
 
-        xc = vt2.v.x, // * rw2,
-        yc = vt2.v.y, // * rw2,
-        zc = vt2.v.z, // * rw2,
-        wc = vt2.v.w, // * rw1,
+        xb = vt1.v.x,
+        yb = vt1.v.y,
+        zb = vt1.v.z,
+        wb = vt1.v.w,
 
-        ua = vt0.uv.x, // * rwa,
-        ub = vt1.uv.x, // * rwa,
-        uc = vt2.uv.x, // * rwb,
+        xc = vt2.v.x,
+        yc = vt2.v.y,
+        zc = vt2.v.z,
+        wc = vt2.v.w,
 
-        va = vt0.uv.y, // * rwb,
-        vb = vt1.uv.y, // * rwc,
-        vc = vt2.uv.y; // * rwc;
-    std::array<std::array<MyFixedPt, 2>, 2>
+        ua = vt0.uv.x,
+        ub = vt1.uv.x,
+        uc = vt2.uv.x,
+
+        va = vt0.uv.y,
+        vb = vt1.uv.y,
+        vc = vt2.uv.y;
+    static constexpr Vec2<size_t> MY_SIZE_2D = {2u, 2u};
+    std::array<std::array<MyFixedPt, MY_SIZE_2D.y>, MY_SIZE_2D.x>
         _b_numer,
         _c_numer,
         _denom;
+    auto do_print_mat2x2 = [](
+        const char* name,
+        const std::array<
+            std::array<MyFixedPt, MY_SIZE_2D.y>,
+            MY_SIZE_2D.x
+        >& val
+    ) -> void {
+        my_printout(name, "=[");
+        for (size_t j=0; j<MY_SIZE_2D.y; ++j) {
+            my_printout("[");
+            for (size_t i=0; i<MY_SIZE_2D.x; ++i) {
+                my_printout(val[j][i]);
+                if (i + 1u < MY_SIZE_2D.x) {
+                    my_printout(" ");
+                }
+            }
+            if (j + 1u < MY_SIZE_2D.y) {
+                my_printout(" ");
+            }
+            my_printout("]");
+        }
+        my_printout("]");
+    };
     _b_numer = {
         {
             {xa - xc, x - xc},
@@ -125,6 +129,7 @@ BaryLerp::BaryLerp(
             {yb - ya, yc - ya},
         }
     };
+
     auto det = [](
         const std::array<std::array<MyFixedPt, 2>, 2>& mat2x2
     ) -> MyFixedPt {
@@ -142,17 +147,72 @@ BaryLerp::BaryLerp(
     B = mult_cx_rw(b_numer_det, one_over_denom_det),
     C = mult_cx_rw(c_numer_det, one_over_denom_det),
     A = MyFixedPt(1) - B - C;
+
     my_printout(
         "BaryLerp: ",
         "A=", A, " ",
         "B=", B, " ",
         "C=", C, "\n"
     );
+    do_print_mat2x2("_b_numer", _b_numer);
+    my_printout(" ");
+    do_print_mat2x2("_c_numer", _c_numer);
+    my_printout(" ");
+    do_print_mat2x2("_denom", _denom);
+    my_printout("\n");
     if (!(
         A < MyFixedPt(0) || A > MyFixedPt(1)
         || B < MyFixedPt(0) || B > MyFixedPt(1)
         || C < MyFixedPt(0) || C > MyFixedPt(1)
     )) {
+        my_printout(
+            "rwa=", rwa, " ",
+            "rwb=", rwb, " ",
+            "rwc=", rwc, "\n"
+        );
+        my_printout(
+            "x=", x, " ",
+            "xa=", xa, " ",
+            "xb=", xb, " ",
+            "xc=", xc, "\n"
+        );
+        my_printout(
+            "y=", y, " ",
+            "ya=", ya, " ",
+            "yb=", yb, " ",
+            "yc=", yc, "\n"
+        );
+        my_printout(
+            //"z=", z, " ",
+            "za=", za, " ",
+            "zb=", zb, " ",
+            "zc=", zc, "\n"
+        );
+        my_printout(
+            //"w=", w, " ",
+            "wa=", wa, " ",
+            "wb=", wb, " ",
+            "wc=", wc, "\n"
+        );
+        my_printout(
+            //"u=", u, " ",
+            "ua=", ua, " ",
+            "ub=", ub, " ",
+            "uc=", uc, "\n"
+        );
+        my_printout(
+            //"v=", v, " ",
+            "va=", va, " ",
+            "vb=", vb, " ",
+            "vc=", vc, "\n"
+        );
+        my_printout(
+            "BaryLerp: ",
+            "b_numer_det=", b_numer_det, " ",
+            "c_numer_det=", c_numer_det, " ",
+            "denom_det=", denom_det, " ",
+            "one_over_denom_det=", one_over_denom_det, "\n"
+        );
         my_printout("BaryLerp: _inside_tri = true;\n");
         _inside_tri = true;
     } else { // if (do_rast_interp)
